d0ffdd767074ec55d70ce8bb6593e557460cabd1
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-08 17:08:13 +0900

    Update: use monix.observable

--- persistence/src/main/scala/com/nulabinc/backlog/c2b/persistence/dsl/StoreADT.scala
+++ persistence/src/main/scala/com/nulabinc/backlog/c2b/persistence/dsl/StoreADT.scala
@@ -1,28 +1,29 @@
 package com.nulabinc.backlog.c2b.persistence.dsl
 
 import com.nulabinc.backlog.c2b.domains._
+import monix.reactive.Observable
 
 sealed trait StoreADT[A]
 
 case class Pure[A](a: A) extends StoreADT[A]
 
-case object GetUsers extends StoreADT[Seq[CybozuUser]]
+case object GetUsers extends StoreADT[Observable[CybozuUser]]
 case class StoreUser(user: CybozuUser) extends StoreADT[Unit]
 
-case class GetIssues(limit: Int, start: Int = 0, step: Int = 100) extends StoreADT[Seq[CybozuIssue]]
+case class GetIssues(limit: Int, start: Int = 0, step: Int = 100) extends StoreADT[Observable[CybozuIssue]]
 case class StoreIssue(issue: CybozuIssue) extends StoreADT[Unit]
 
-case class GetIssueComments(issue: CybozuIssue) extends StoreADT[Seq[CybozuComment]]
+case class GetIssueComments(issue: CybozuIssue) extends StoreADT[Observable[CybozuComment]]
 case class StoreIssueComment(issue: CybozuIssue, comment: CybozuComment) extends StoreADT[Unit]
 
-case object GetPriorities extends StoreADT[Seq[CybozuPriority]]
+case object GetPriorities extends StoreADT[Observable[CybozuPriority]]
 case class StorePriority(priority: CybozuPriority) extends StoreADT[Unit]
 
-case object GetStatuses extends StoreADT[Seq[CybozuStatus]]
-case class StoreStatuse(status: CybozuStatus) extends StoreADT[Unit]
+case object GetStatuses extends StoreADT[Observable[CybozuStatus]]
+case class StoreStatus(status: CybozuStatus) extends StoreADT[Unit]
 
-case class GetEvents(limit: Int, start: Int = 0, step: Int = 100) extends StoreADT[Seq[CybozuEvent]]
+case class GetEvents(limit: Int, start: Int = 0, step: Int = 100) extends StoreADT[Observable[CybozuEvent]]
 case class StoreEvent(event: CybozuEvent) extends StoreADT[Unit]
 
-case class GetForums(limit: Int, start: Int = 0, step: Int = 100) extends StoreADT[Seq[CybozuForum]]
+case class GetForums(limit: Int, start: Int = 0, step: Int = 100) extends StoreADT[Observable[CybozuForum]]
 case class StoreForum(forum: CybozuForum) extends StoreADT[Unit]
--- persistence/src/main/scala/com/nulabinc/backlog/c2b/persistence/dsl/StoreDSL.scala
+++ persistence/src/main/scala/com/nulabinc/backlog/c2b/persistence/dsl/StoreDSL.scala
@@ -2,6 +2,7 @@ package com.nulabinc.backlog.c2b.persistence.dsl
 
 import cats.free.Free
 import com.nulabinc.backlog.c2b.domains._
+import monix.reactive.Observable
 
 object StoreDSL {
 
@@ -10,43 +11,43 @@ object StoreDSL {
   def pure[A](a: A): StoreProgram[A] =
     Free.liftF(Pure(a))
 
-  def getUsers: StoreProgram[Seq[CybozuUser]] =
+  def getUsers: StoreProgram[Observable[CybozuUser]] =
     Free.liftF(GetUsers)
 
   def storeUser(user: CybozuUser): StoreProgram[Unit] =
     Free.liftF(StoreUser(user))
 
-  def getIssues(limit: Int, start: Int = 0, step: Int = 100): StoreProgram[Seq[CybozuUser]] =
-    Free.liftF(GetUsers(limit, start, step))
+  def getIssues(limit: Int, start: Int = 0, step: Int = 100): StoreProgram[Observable[CybozuIssue]] =
+    Free.liftF(GetIssues(limit, start, step))
 
   def storeIssue(issue: CybozuIssue): StoreProgram[Unit] =
     Free.liftF(StoreIssue(issue))
 
-  def getIssueComments(issue: CybozuIssue): StoreProgram[Seq[CybozuComment]] =
+  def getIssueComments(issue: CybozuIssue): StoreProgram[Observable[CybozuComment]] =
     Free.liftF(GetIssueComments(issue))
 
   def storeIssueComment(issue: CybozuIssue, comment: CybozuComment): StoreProgram[Unit] =
     Free.liftF(StoreIssueComment(issue, comment))
 
-  def getPriorities: StoreProgram[Seq[CybozuPriority]] =
+  def getPriorities: StoreProgram[Observable[CybozuPriority]] =
     Free.liftF(GetPriorities)
 
   def storePriority(priority: CybozuPriority): StoreProgram[Unit] =
     Free.liftF(StorePriority(priority))
 
-  def getStatuses: StoreProgram[Seq[CybozuStatus]] =
+  def getStatuses: StoreProgram[Observable[CybozuStatus]] =
     Free.liftF(GetStatuses)
 
   def storeStatus(status: CybozuStatus): StoreProgram[Unit] =
-    Free.liftF(StoreStatuse(status))
+    Free.liftF(StoreStatus(status))
 
-  def getEvents(limit: Int, start: Int = 0, step: Int = 100): StoreProgram[Seq[CybozuEvent]] =
+  def getEvents(limit: Int, start: Int = 0, step: Int = 100): StoreProgram[Observable[CybozuEvent]] =
     Free.liftF(GetEvents(limit, start, step))
 
   def storeEvent(event: CybozuEvent): StoreProgram[Unit] =
     Free.liftF(StoreEvent(event))
 
-  def getForums(limit: Int, start: Int = 0, step: Int = 100): StoreProgram[Seq[CybozuForum]] =
+  def getForums(limit: Int, start: Int = 0, step: Int = 100): StoreProgram[Observable[CybozuForum]] =
     Free.liftF(GetForums(limit, start, step))
 
   def storeForum(forum: CybozuForum): StoreProgram[Unit] =
