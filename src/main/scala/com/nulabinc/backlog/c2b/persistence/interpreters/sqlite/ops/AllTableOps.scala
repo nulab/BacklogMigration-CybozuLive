2c360fb65b1cdcad667bafcae8afd23bae7917e3
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-12 10:47:17 +0900

    Add: all table ops

---
+++ src/main/scala/com/nulabinc/backlog/c2b/persistence/interpreters/sqlite/ops/AllTableOps.scala
@@ -0,0 +1,9 @@
+package com.nulabinc.backlog.c2b.persistence.interpreters.sqlite.ops
+
+private[sqlite] case class AllTableOps() {
+  val commentTableOps = CommentTableOps()
+  val eventTableOps = EventTableOps()
+  val forumTableOps = ForumTableOps()
+  val issueTableOps = IssueTableOps()
+  val userTableOps = UserTableOps()
+}
