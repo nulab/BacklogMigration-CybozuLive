b3d1a765d41486e149dd2d12be5479bec3ac8c75
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-13 11:37:07 +0900

    Clean: common base issue

--- src/main/scala/com/nulabinc/backlog/c2b/converters/IssueConverter.scala
+++ src/main/scala/com/nulabinc/backlog/c2b/converters/IssueConverter.scala
@@ -13,6 +13,8 @@ object IssueConverter {
 
    */
 
+    val ISSUE_TYPE_NAME = "ToDoリスト"
+
     for {
       createdUser <- UserConverter.toBacklogUser(issue.creatorId)
       updaterUser <- UserConverter.toBacklogUser(issue.updaterId)
@@ -21,37 +23,54 @@ object IssueConverter {
       priority <- ctx.getPriorityName(issue.priority)
     } yield {
       Right(
-        BacklogIssue(
-          eventType = "issue",
-          id = issue.id,
-          optIssueKey = None,
-          summary = BacklogIssueSummary(value = issue.title, original = issue.title),
-          optParentIssueId = None,
-          description = issue.content,
-          optStartDate = None,
-          optDueDate = issue.dueDate.map(DateUtil.toDateString),
-          optEstimatedHours = None,
-          optActualHours = None,
-          optIssueTypeName = None,
-          statusName = status,
-          categoryNames = Seq.empty[String],
-          versionNames = Seq.empty[String],
-          milestoneNames = Seq.empty[String],
-          priorityName = priority,
-          optAssignee = maybeAssignee,
-          attachments = Seq.empty[BacklogAttachment],
-          sharedFiles = Seq.empty[BacklogSharedFile],
-          customFields = Seq.empty[BacklogCustomField],
-          notifiedUsers = Seq.empty[BacklogUser],
-          operation = BacklogOperation(
-            optCreatedUser = Some(createdUser),
-            optCreated = Some(DateUtil.toDateTimeString(issue.createdAt)),
-            optUpdatedUser = Some(updaterUser),
-            optUpdated = Some(DateUtil.toDateTimeString(issue.updatedAt))
+        defaultBacklogIssue.copy(
+          id                = issue.id,
+          summary           = BacklogIssueSummary(value = issue.title, original = issue.title),
+          description       = issue.content,
+          optDueDate        = issue.dueDate.map(DateUtil.toDateString),
+          optIssueTypeName  = Some(ISSUE_TYPE_NAME),
+          statusName        = status,
+          priorityName      = priority,
+          optAssignee       = maybeAssignee,
+          operation         = BacklogOperation(
+            optCreatedUser    = Some(createdUser),
+            optCreated        = Some(DateUtil.toDateTimeString(issue.createdAt)),
+            optUpdatedUser    = Some(updaterUser),
+            optUpdated        = Some(DateUtil.toDateTimeString(issue.updatedAt))
           )
         )
       )
     }
-
   }
+
+  private val defaultBacklogIssue: BacklogIssue =
+    BacklogIssue(
+      eventType = "issue",
+      id = 0,
+      optIssueKey = None,
+      summary = BacklogIssueSummary(value = "", original = ""),
+      optParentIssueId = None,
+      description = "",
+      optStartDate = None,
+      optDueDate = None,
+      optEstimatedHours = None,
+      optActualHours = None,
+      optIssueTypeName = None,
+      statusName = "",
+      categoryNames = Seq.empty[String],
+      versionNames = Seq.empty[String],
+      milestoneNames = Seq.empty[String],
+      priorityName = "",
+      optAssignee = None,
+      attachments = Seq.empty[BacklogAttachment],
+      sharedFiles = Seq.empty[BacklogSharedFile],
+      customFields = Seq.empty[BacklogCustomField],
+      notifiedUsers = Seq.empty[BacklogUser],
+      operation = BacklogOperation(
+        optCreatedUser = None,
+        optCreated = None,
+        optUpdatedUser = None,
+        optUpdated = None
+      )
+    )
 }
