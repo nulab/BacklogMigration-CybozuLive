450f1540aef4c05761590e8b81d3cbe390d9e068
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-13 12:17:36 +0900

    WIP: sql interpreter

--- src/main/scala/com/nulabinc/backlog/c2b/persistence/interpreters/sqlite/SQLiteInterpreter.scala
+++ src/main/scala/com/nulabinc/backlog/c2b/persistence/interpreters/sqlite/SQLiteInterpreter.scala
@@ -1,13 +1,16 @@
 package com.nulabinc.backlog.c2b.persistence.interpreters.sqlite
 
-import com.nulabinc.backlog.c2b.persistence.dsl.{Pure, StoreADT}
+import com.nulabinc.backlog.c2b.persistence.dsl.{GetUsers, Pure, StoreADT}
 import com.nulabinc.backlog.c2b.persistence.dsl.StoreDSL.StoreProgram
 import com.nulabinc.backlog.c2b.persistence.interpreters.DBInterpreter
 import monix.eval.Task
 import cats.Monad
+import com.nulabinc.backlog.c2b.persistence.interpreters.sqlite.ops.AllTableOps
 import slick.jdbc.SQLiteProfile.api._
 
 case class SQLiteInterpreter(configPath: String) extends DBInterpreter {
+
+  val allTableOps = AllTableOps()
 
   private val db = Database.forConfig(configPath)
 
@@ -15,11 +18,19 @@ case class SQLiteInterpreter(configPath: String) extends DBInterpreter {
 
   override def run[A](prg: StoreProgram[A]): Task[A] =
     prg.foldMap(this)
-  
-  override def apply[A](fa: StoreADT[A]): Task[A] = ???
 
-//  override def apply[A](fa: StoreADT[A]): DBIO[A] = fa match {
-//    case Pure(a) => DBIO.successful(a)
-//  }
+  override def apply[A](fa: StoreADT[A]): Task[A] = {
+
+    import allTableOps._
+
+    fa match {
+      case Pure(a) => Task.deferFuture {
+        db.run(DBIO.successful(a))
+      }
+      case GetUsers(offset, limit) => Task.deferFuture {
+        db.run(userTableOps.selectAll(offset, limit))
+      }
+    }
+  }
 
 }
