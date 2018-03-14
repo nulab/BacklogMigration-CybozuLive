f50a7e23c46d3b32156ac2c6b1f2941b37993ae0
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-14 11:23:12 +0900

    Disable: convert ADT. Not need

--- src/main/scala/com/nulabinc/backlog/c2b/converters/dsl/ConvertADT.scala
+++ src/main/scala/com/nulabinc/backlog/c2b/converters/dsl/ConvertADT.scala
@@ -1,9 +1,9 @@
-package com.nulabinc.backlog.c2b.converters.dsl
-
-import com.nulabinc.backlog.c2b.datas._
-import com.nulabinc.backlog.migration.common.domain._
-
-sealed trait ConvertADT[A]
-
-case class ConvertToBacklogUser(cybozuUser: CybozuUser) extends ConvertADT[BacklogUser]
-case class ConvertToBacklogIssue(cybozuIssue: CybozuIssue) extends ConvertADT[BacklogIssue]
+//package com.nulabinc.backlog.c2b.converters.dsl
+//
+//import com.nulabinc.backlog.c2b.datas._
+//import com.nulabinc.backlog.migration.common.domain._
+//
+//sealed trait ConvertADT[A]
+//
+//case class ConvertToBacklogUser(cybozuUser: CybozuUser) extends ConvertADT[BacklogUser]
+//case class ConvertToBacklogIssue(cybozuIssue: CybozuIssue) extends ConvertADT[BacklogIssue]
