fa30efc4678cc5e8b2e159eeab2f440692fd4776
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-14 16:13:38 +0900

    Update: refactor classes

--- src/main/scala/com/nulabinc/backlog/c2b/converters/IssueConverter.scala
+++ src/main/scala/com/nulabinc/backlog/c2b/converters/IssueConverter.scala
@@ -5,65 +5,88 @@ import com.nulabinc.backlog.c2b.datas.{CybozuEvent, CybozuForum, CybozuIssue, Cy
 import com.nulabinc.backlog.migration.common.domain._
 
 
-case class BacklogIssueParams(issue: CybozuIssue,
-                              creator: CybozuUser,
-                              updater: CybozuUser,
-                              maybeAssignee: Option[CybozuUser])
+sealed trait IssueFrom
+
+case class FromCybozuIssue(
+  issue: CybozuIssue,
+  creator: CybozuUser,
+  updater: CybozuUser,
+  maybeAssignee: Option[CybozuUser]
+) extends IssueFrom
+
+case class FromCybozuEvent(
+  event: CybozuEvent,
+  creator: CybozuUser
+) extends IssueFrom
+
+case class FromCybozuForum(
+  forum: CybozuForum,
+  creator: CybozuUser,
+  updater: CybozuUser
+) extends IssueFrom
 
 
-class IssueConverter()(implicit ctx: MappingContext) extends Converter[BacklogIssueParams, BacklogIssue]{
+class IssueConverter()(implicit ctx: MappingContext) extends Converter[IssueFrom, BacklogIssue]{
 
   val userConverter = new UserConverter()
 
-  def to(params: BacklogIssueParams): Either[ConvertError, BacklogIssue] = {
+  override def to(issueFrom: IssueFrom): Either[ConvertError, BacklogIssue] = issueFrom match {
+    case fromCybozuIssue: FromCybozuIssue =>
+      from(fromCybozuIssue)
+    case fromCybozuEvent: FromCybozuEvent =>
+      from(fromCybozuEvent)
+    case fromCybozuForum: FromCybozuForum =>
+      from(fromCybozuForum)
+  }
+
+  def from(fromCybozuIssue: FromCybozuIssue): Either[ConvertError, BacklogIssue] = {
 
     val ISSUE_TYPE_NAME = "ToDoリスト"
 
     for {
-      convertedCreator <- userConverter.to(params.creator)
-      convertedUpdater <- userConverter.to(params.updater)
-      maybeConvertedAssignee <- userConverter.to(params.maybeAssignee)
+      convertedCreator <- userConverter.to(fromCybozuIssue.creator)
+      convertedUpdater <- userConverter.to(fromCybozuIssue.updater)
+      maybeConvertedAssignee <- userConverter.to(fromCybozuIssue.maybeAssignee)
       t = maybeConvertedAssignee
-      status <- ctx.getStatusName(params.issue.status)
-      priority <- ctx.getPriorityName(params.issue.priority)
+      status <- ctx.getStatusName(fromCybozuIssue.issue.status)
+      priority <- ctx.getPriorityName(fromCybozuIssue.issue.priority)
     } yield {
         defaultBacklogIssue.copy(
-          id                = params.issue.id,
-          summary           = BacklogIssueSummary(value = params.issue.title, original = params.issue.title),
-          description       = params.issue.content,
-          optDueDate        = params.issue.dueDate.map(DateUtil.toDateString),
+          id                = fromCybozuIssue.issue.id,
+          summary           = BacklogIssueSummary(value = fromCybozuIssue.issue.title, original = fromCybozuIssue.issue.title),
+          description       = fromCybozuIssue.issue.content,
+          optDueDate        = fromCybozuIssue.issue.dueDate.map(DateUtil.toDateString),
           optIssueTypeName  = Some(ISSUE_TYPE_NAME),
           statusName        = status,
           priorityName      = priority,
           optAssignee       = maybeConvertedAssignee,
           operation         = BacklogOperation(
             optCreatedUser    = Some(convertedCreator),
-            optCreated        = Some(DateUtil.toDateTimeString(params.issue.createdAt)),
+            optCreated        = Some(DateUtil.toDateTimeString(fromCybozuIssue.issue.createdAt)),
             optUpdatedUser    = Some(convertedUpdater),
-            optUpdated        = Some(DateUtil.toDateTimeString(params.issue.updatedAt))
+            optUpdated        = Some(DateUtil.toDateTimeString(fromCybozuIssue.issue.updatedAt))
           )
         )
     }
   }
 
-  def toBacklogIssue(event: CybozuEvent,
-                     creator: CybozuUser): Either[ConvertError, BacklogIssue] = {
+  def from(fromCybozuEvent: FromCybozuEvent): Either[ConvertError, BacklogIssue] = {
 
     val ISSUE_TYPE_NAME = "イベント"
 
     for {
-      convertedCreator <- userConverter.to(creator)
+      convertedCreator <- userConverter.to(fromCybozuEvent.creator)
     } yield {
       defaultBacklogIssue.copy(
-        id                = event.id,
-        summary           = BacklogIssueSummary(value = event.title, original = event.title),
-        description       = event.memo + "\n\n" + event.menu,
+        id                = fromCybozuEvent.event.id,
+        summary           = createBacklogIssueSummary(fromCybozuEvent.event.title),
+        description       = fromCybozuEvent.event.memo + "\n\n" + fromCybozuEvent.event.menu,
         optStartDate      = None,
         optDueDate        = None,
         optIssueTypeName  = Some(ISSUE_TYPE_NAME),
         operation         = BacklogOperation(
           optCreatedUser    = Some(convertedCreator),
-          optCreated        = Some(DateUtil.toDateTimeString(event.startDateTime)),
+          optCreated        = Some(DateUtil.toDateTimeString(fromCybozuEvent.event.startDateTime)),
           optUpdatedUser    = None,
           optUpdated        = None
         )
@@ -71,32 +94,33 @@ class IssueConverter()(implicit ctx: MappingContext) extends Converter[BacklogIs
     }
   }
 
-  def toBacklogIssue(forum: CybozuForum,
-                     creator: CybozuUser,
-                     updater: CybozuUser): Either[ConvertError, BacklogIssue] = {
+  def from(fromCybozuForum: FromCybozuForum): Either[ConvertError, BacklogIssue] = {
 
     val ISSUE_TYPE_NAME = "掲示板"
 
     for {
-      convertedCreator <- userConverter.to(creator)
-      convertedUpdater <- userConverter.to(updater)
+      convertedCreator <- userConverter.to(fromCybozuForum.creator)
+      convertedUpdater <- userConverter.to(fromCybozuForum.updater)
     } yield {
       defaultBacklogIssue.copy(
-        id                = forum.id,
-        summary           = BacklogIssueSummary(value = forum.title, original = forum.title),
-        description       = forum.content,
+        id                = fromCybozuForum.forum.id,
+        summary           = BacklogIssueSummary(value = fromCybozuForum.forum.title, original = fromCybozuForum.forum.title),
+        description       = fromCybozuForum.forum.content,
         optStartDate      = None,
         optDueDate        = None,
         optIssueTypeName  = Some(ISSUE_TYPE_NAME),
         operation         = BacklogOperation(
           optCreatedUser    = Some(convertedCreator),
-          optCreated        = Some(DateUtil.toDateTimeString(forum.createdAt)),
+          optCreated        = Some(DateUtil.toDateTimeString(fromCybozuForum.forum.createdAt)),
           optUpdatedUser    = Some(convertedUpdater),
-          optUpdated        = Some(DateUtil.toDateTimeString(forum.updatedAt))
+          optUpdated        = Some(DateUtil.toDateTimeString(fromCybozuForum.forum.updatedAt))
         )
       )
     }
   }
+
+  private def createBacklogIssueSummary(summary: String): BacklogIssueSummary =
+    BacklogIssueSummary(value = summary, original = summary)
 
   private val defaultBacklogIssue: BacklogIssue =
     BacklogIssue(
