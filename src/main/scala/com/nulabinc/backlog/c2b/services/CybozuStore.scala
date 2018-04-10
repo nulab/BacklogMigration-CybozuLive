2f1e0593b8f71029e1447e17bfe2be59c53e4b74
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-04-10 10:47:40 +0900

    Update: display csv process

--- src/main/scala/com/nulabinc/backlog/c2b/services/CybozuStore.scala
+++ src/main/scala/com/nulabinc/backlog/c2b/services/CybozuStore.scala
@@ -2,15 +2,17 @@ package com.nulabinc.backlog.c2b.services
 
 import java.io.File
 
+import com.nulabinc.backlog.c2b.core.Logger
 import com.nulabinc.backlog.c2b.datas._
 import com.nulabinc.backlog.c2b.datas.Types.AnyId
-import com.nulabinc.backlog.c2b.interpreters.AppDSL
+import com.nulabinc.backlog.c2b.interpreters.{AppDSL, ConsoleDSL}
 import com.nulabinc.backlog.c2b.interpreters.AppDSL.AppProgram
 import com.nulabinc.backlog.c2b.persistence.dsl.{Insert, StoreDSL}
 import com.nulabinc.backlog.c2b.persistence.dsl.StoreDSL.StoreProgram
 import com.nulabinc.backlog.c2b.readers.CybozuCSVReader
+import com.osinka.i18n.Messages
 
-object CybozuStore {
+object CybozuStore extends Logger {
 
   def all(csvFiles: Array[File]): AppProgram[Unit] = {
     val todoFiles = {
@@ -19,7 +21,7 @@ object CybozuStore {
     }
     val eventFiles = {
       csvFiles.filter(_.getName.contains("live_Events_")) ++
-        csvFiles.filter(_.getName.contains("live_イベント_"))
+      csvFiles.filter(_.getName.contains("live_イベント_"))
     }
     val forumFiles = csvFiles.filter(_.getName.contains("live_掲示板_")) // TODO: english version
 
@@ -30,88 +32,91 @@ object CybozuStore {
     } yield ()
   }
 
-  def todo(files: Array[File]): AppProgram[Unit] = {
-    val dbProgram = for {
-      _ <- StoreDSL.writeDBStream(
-        CybozuCSVReader.toCybozuTodo(files).map { result =>
-          val creator = CybozuDBUser.from(result.issue.creator)
-          val updater = CybozuDBUser.from(result.issue.updater)
-          val assignees = result.issue.assignees.map(u => CybozuDBUser.from(u))
-          for {
-            // Save issues
-            creatorId <- insertOrUpdateUser(creator)
-            updaterId <- insertOrUpdateUser(updater)
-            issueId <- {
-              val issue = CybozuDBTodo.from(
-                todo = result.issue,
-                creatorId = creatorId,
-                updaterId = updaterId
-              )
-              StoreDSL.storeTodo(issue)
-            }
-            // Save assignees
-            assigneeIdsProgram = assignees.map(insertOrUpdateUser)
-            assigneeIds <- sequential(assigneeIdsProgram)
-            _ <- StoreDSL.storeTodoAssignees(issueId, assigneeIds)
-            // Save comments
-            _ <- comments(issueId, result.comments)
-          } yield ()
-        }
+  def todo(files: Array[File]): AppProgram[Unit] =
+    for {
+      _ <- AppDSL.fromConsole(ConsoleDSL.print(Messages("message.init.collect", Messages("issue.type.todo"))))
+      _ <- AppDSL.fromDB(
+        StoreDSL.writeDBStream(
+          CybozuCSVReader.toCybozuTodo(files).map { result =>
+            val creator = CybozuDBUser.from(result.issue.creator)
+            val updater = CybozuDBUser.from(result.issue.updater)
+            val assignees = result.issue.assignees.map(u => CybozuDBUser.from(u))
+            for {
+              // Save issues
+              creatorId <- insertOrUpdateUser(creator)
+              updaterId <- insertOrUpdateUser(updater)
+              issueId <- {
+                val issue = CybozuDBTodo.from(
+                  todo = result.issue,
+                  creatorId = creatorId,
+                  updaterId = updaterId
+                )
+                StoreDSL.storeTodo(issue)
+              }
+              // Save assignees
+              assigneeIdsProgram = assignees.map(insertOrUpdateUser)
+              assigneeIds <- sequential(assigneeIdsProgram)
+              _ <- StoreDSL.storeTodoAssignees(issueId, assigneeIds)
+              // Save comments
+              _ <- comments(issueId, result.comments)
+            } yield ()
+          }
+        )
       )
     } yield ()
-    AppDSL.fromDB(dbProgram)
-  }
 
-  def event(files: Array[File]): AppProgram[Unit] = {
-    val dbProgram = for {
-      _ <- StoreDSL.writeDBStream(
-        CybozuCSVReader.toCybozuEvent(files).map { result =>
-          val creator = CybozuDBUser.from(result.issue.creator)
-          for {
-            // Save event
-            creatorId <- insertOrUpdateUser(creator)
-            eventId <- {
-              val issue = CybozuDBEvent.from(
-                event = result.issue,
-                creatorId = creatorId
-              )
-              StoreDSL.storeEvent(issue)
-            }
-            // Save comments
-            _ <- comments(eventId, result.comments)
-          } yield ()
-        }
+  def event(files: Array[File]): AppProgram[Unit] =
+    for {
+      _ <- AppDSL.fromConsole(ConsoleDSL.print(Messages("message.init.collect", Messages("issue.type.event"))))
+      _ <- AppDSL.fromDB(
+        StoreDSL.writeDBStream(
+          CybozuCSVReader.toCybozuEvent(files).map { result =>
+            val creator = CybozuDBUser.from(result.issue.creator)
+            for {
+              // Save event
+              creatorId <- insertOrUpdateUser(creator)
+              eventId <- {
+                val issue = CybozuDBEvent.from(
+                  event = result.issue,
+                  creatorId = creatorId
+                )
+                StoreDSL.storeEvent(issue)
+              }
+              // Save comments
+              _ <- comments(eventId, result.comments)
+            } yield ()
+          }
+        )
       )
     } yield ()
-    AppDSL.fromDB(dbProgram)
-  }
 
-  def forum(files: Array[File]): AppProgram[Unit] = {
-    val dbProgram = for {
-      _ <- StoreDSL.writeDBStream(
-        CybozuCSVReader.toCybozuForum(files).map { result =>
-          val creator = CybozuDBUser.from(result.issue.creator)
-          val updater = CybozuDBUser.from(result.issue.updater)
-          for {
-            // Save event
-            creatorId <- insertOrUpdateUser(creator)
-            updaterId <- insertOrUpdateUser(updater)
-            forumId <- {
-              val forum = CybozuDBForum.from(
-                forum = result.issue,
-                creatorId = creatorId,
-                updaterId = updaterId
-              )
-              StoreDSL.storeForum(forum)
-            }
-            // Save comments
-            _ <- comments(forumId, result.comments)
-          } yield ()
-        }
+  def forum(files: Array[File]): AppProgram[Unit] =
+    for {
+      _ <- AppDSL.fromConsole(ConsoleDSL.print(Messages("message.init.collect", Messages("issue.type.forum"))))
+      _ <- AppDSL.fromDB(
+        StoreDSL.writeDBStream(
+          CybozuCSVReader.toCybozuForum(files).map { result =>
+            val creator = CybozuDBUser.from(result.issue.creator)
+            val updater = CybozuDBUser.from(result.issue.updater)
+            for {
+              // Save event
+              creatorId <- insertOrUpdateUser(creator)
+              updaterId <- insertOrUpdateUser(updater)
+              forumId <- {
+                val forum = CybozuDBForum.from(
+                  forum = result.issue,
+                  creatorId = creatorId,
+                  updaterId = updaterId
+                )
+                StoreDSL.storeForum(forum)
+              }
+              // Save comments
+              _ <- comments(forumId, result.comments)
+            } yield ()
+          }
+        )
       )
     } yield ()
-    AppDSL.fromDB(dbProgram)
-  }
 
   private def sequential[A](prgs: Seq[StoreProgram[A]]): StoreProgram[Seq[A]] =
     prgs.foldLeft(StoreDSL.pure(Seq.empty[A])) {
